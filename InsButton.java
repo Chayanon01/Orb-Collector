10f94bb6e345dceabf60fa82d55ac7b42c7fb4ec
Chayanon01 <chayanon.tree@mail.kmutt.ac.th>
2017-03-14 21:53:43 +0700

    Add files via upload

--- InsButton.java
+++ InsButton.java
@@ -15,8 +15,9 @@ public class InsButton extends Button
     public void act() 
     {
        if(Greenfoot.mouseClicked(this)){
-            getWorld().addObject(new Instruction(), 485, 260);
-            getWorld().addObject(new back(), 100, 80);
+            getWorld().addObject(new Control(), 485, 260);
+            getWorld().addObject(new Next(), 825, 450);
+            getWorld().addObject(new back(), 100, 450);
        }
     }    
 }
